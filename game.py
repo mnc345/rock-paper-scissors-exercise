240f109a7786baa0bc4525e6e404456621abe26a
mnc345 <mnc345@stern.nyu.edu>
2021-05-25 21:13:12 -0400

    Displaying results

--- game.py
+++ game.py
@@ -1,21 +1,47 @@
 # game.py
+print("Welcome 'Player One' to my Rock-Paper-Scissors game...")
 
 import random
 
-print("Rock, Paper, Scissors, Shoot!")
+user_choice = input("Please choose either 'rock', 'paper', or 'scissors': ")
 
-user_choice = input("Please choose one of 'rock', 'paper', 'scissors': ")
-
-print("USER CHOICE: ", user_choice)
+print("You chose: ", user_choice)
 
 if (user_choice == "rock") or (user_choice == "paper") or (user_choice == "scissors"):
-    print("VALID. KEEP GOING")
+    print("VALID. Keep going.")
 else:
     print("OOPS, invalid input. Please try again.")
     exit()
 
 valid_options = ["rock", "paper", "scissors"]
 computer_choice = random.choice(valid_options)
-print("COMPUTER CHOICE: ", computer_choice)
+print("The computer chose: ", computer_choice)
 
-print("THIS IS THE END OF OUR GAME. PLEASE PLAY AGAIN.")
+if (user_choice == "rock") and (computer_choice == "rock"):
+    print("WE HAVE A TIE!")
+
+if (user_choice == "rock") and (computer_choice == "scissors"):
+    print("CONGRATULATIONS! YOU WON!")
+
+if (user_choice == "rock") and (computer_choice == "paper"):
+    print("UH OH! The computer won. Better luck next time!")
+
+if (user_choice == "paper") and (computer_choice == "paper"):
+    print("WE HAVE A TIE!")
+
+if (user_choice == "paper") and (computer_choice == "scissors"):
+    print("UH OH! The computer won. Better luck next time!")
+
+if (user_choice == "paper") and (computer_choice == "rock"):
+    print("CONGRATULATIONS! YOU WON!")
+
+if (user_choice == "scissors") and (computer_choice == "scissors"):
+    print("WE HAVE A TIE!")
+
+if (user_choice == "scissors") and (computer_choice == "rock"):
+    print("UH OH! The computer won. Better luck next time!")
+
+if (user_choice == "scissors") and (computer_choice == "paper"):
+    print("CONGRATULATIONS! YOU WON!")
+
+print("THANK YOU FOR PLAYING! PLEASE PLAY AGAIN.")
