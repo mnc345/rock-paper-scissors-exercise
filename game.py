63c22ba687289ea725eb8f2642d735e2dba49dc2
mnc345 <mnc345@stern.nyu.edu>
2021-05-25 20:43:02 -0400

    Validated user inputs

--- game.py
+++ game.py
@@ -1,7 +1,14 @@
 # game.py
-
 print("Rock, Paper, Scissors, Shoot!")
 
-user_choice = input("Please choose one 'rock', 'paper', 'scissors':")
+user_choice = input("Please choose one of 'rock', 'paper', 'scissors': ")
 
 print("USER CHOICE: ", user_choice)
+
+if (user_choice == "rock") or (user_choice == "paper") or (user_choice == "scissors"):
+    print("VALID. KEEP GOING")
+else:
+    print("OOPS, invalid input. Please try again.")
+    exit()
+    
+print("THIS IS THE END OF OUR GAME. PLEASE PLAY AGAIN.")
